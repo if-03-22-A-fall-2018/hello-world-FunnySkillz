ec220d1ea96fbbea74555c9edf591a4337701ad9
Sebastijan Bogdan <43369462+FunnySkillz@users.noreply.github.com>
2018-09-18 10:48:55 +0200

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			HelloWorld
+ * Author:			Sebastijan Bogdan
+ * ----------------------------------------------------------
+ * Description:
+ * Typical HelloWorld program where the output is my name.
+ * e.g. Hello Sebastijan Bogdan.
+ * ----------------------------------------------------------
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  printf("Hello Sebastijan Bogdan!\n");
+  return 0;
+}
